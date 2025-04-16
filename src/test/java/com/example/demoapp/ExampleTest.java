ea40fd377c00f3480c96080a24a74f4b1fa5d91d
xdung04 <157205064+xdung04@users.noreply.github.com>
2025-04-16 08:33:35 +0700

    mm

---
+++ src/test/java/com/example/demoapp/ExampleTest.java
@@ -0,0 +1,13 @@
+package com.example.demoapp;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleTest {
+
+    @Test
+    void simpleAdditionTest() {
+        int result = 2 + 3;
+        assertEquals(5, result, "2 + 3 should equal 5");
+    }
+}
